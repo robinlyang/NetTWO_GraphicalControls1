5dc74b2e6bc3389dcaa7f23f1d981fe77745d790
robinlyang <robiny@gmail.com>
2016-12-05 19:10:36 -0700

    More

--- NetTWO_GraphicalControls1/Form1.Designer.cs
+++ NetTWO_GraphicalControls1/Form1.Designer.cs
@@ -47,6 +47,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             this.button3 = new System.Windows.Forms.Button();
+            this.clock1 = new clock.clock();
             this.contextMenuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.statusStrip1.SuspendLayout();
@@ -185,15 +186,25 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 7;
-            this.button3.Text = "button3";
+            this.button3.Text = " button3";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // clock1
+            // 
+            this.clock1.clock_color = System.Drawing.Color.Red;
+            this.clock1.clock_type = clock.types.longtime;
+            this.clock1.Location = new System.Drawing.Point(771, 12);
+            this.clock1.Name = "clock1";
+            this.clock1.Size = new System.Drawing.Size(92, 29);
+            this.clock1.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(875, 442);
+            this.Controls.Add(this.clock1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.statusStrip1);
@@ -233,6 +244,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ErrorProvider errorProvider1;
         private System.Windows.Forms.Button button3;
+        private clock.clock clock1;
     }
 }
 
